3a40e288614bc6efe851ff08393b0d7966d737a9
bellashilliday <bellashilliday@10.181.38.138>
2019-02-25 22:44:46 -0700

    1st revision of calulator

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,40 @@
+// Name: Isabella Shilliday 
+// @version
+// StudentID: 547
+// Description: goes through each method and does the operation specified 
+// in each method
+
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+}
